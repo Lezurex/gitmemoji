2eb355608cc99d95ab76599ee785381f1ce459f3
Lenny Angst <lenny@familie-angst.ch>
2023-03-22 19:42:06 +0100

    ⬆️ Upgrade dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "csshake";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EasyQuiz from "./EasyQuiz";
 import HardQuiz from "./HardQuiz";
 import Home from "./Home";
@@ -8,17 +8,11 @@ import Home from "./Home";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/easy">
-          <EasyQuiz />
-        </Route>
-        <Route path="/hard">
-          <HardQuiz />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/easy" element={<EasyQuiz />} />
+        <Route path="/hard" element={<HardQuiz />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 };
