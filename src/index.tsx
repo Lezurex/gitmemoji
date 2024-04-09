8cc997ed3c82ddce88c09aa92bb03e89e917a69e
Lenny Angst <lenny@familie-angst.ch>
2024-04-09 22:28:42 +0200

    ♻️ Migrate to Vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container!);
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
